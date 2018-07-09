d4f7d19f1a3e42e5ade5c1e473dd322397e9d5b6
ddearauj <danielpereira03@gmail.com>
2018-07-09 15:55:21 -0300

    linear regression generated data

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -10,22 +10,22 @@ def bivariable_polinomial(coeficients, sample_size=100, x0=0, xmax=100, mu=0, si
 	"""
 
 	# there has to be a better way than to use numbers from 0 to xmax divided by sample size
-    x = np.linspace(x0, xmax, sample_size, endpoint=True)
-    error = np.random.normal(mu, sigma, sample_size)
+	x = np.linspace(x0, xmax, sample_size, endpoint=True)
+	error = np.random.normal(mu, sigma, sample_size)
 
 
 
-    #initialize y as a np array of size=sample_size
-    y = error
-    
-    # here we get the list of coeficients and for each one we will do
-    # coeficient[i]*x^degree
-    # since the enumerate function does not go down, we can make degree as coef.len() - i - 1
+	#initialize y as a np array of size=sample_size
+	y = error
+	
+	# here we get the list of coeficients and for each one we will do
+	# coeficient[i]*x^degree
+	# since the enumerate function does not go down, we can make degree as coef.len() - i - 1
 
-    length = len(coeficients)
-    for degree, coef in enumerate(coeficients):
-    	if length - degree > 1 :
-    		y += coef*np.power(x, (length - degree - 1))
-    	else:
-    		y+= coef
-    return x, y
+	length = len(coeficients)
+	for degree, coef in enumerate(coeficients):
+		if length - degree > 1 :
+			y += coef*np.power(x, (length - degree - 1))
+		else:
+			y+= coef
+	return x, y	
